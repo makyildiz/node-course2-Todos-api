3d66518e56e2ab6d2e081f28a556910118a496a8
Mesut Akyildiz <mesut.akyildiz@gmail.com>
2018-09-04 07:13:55 +0300

    Hash user password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,28 +1,18 @@
-const {SHA256} = require('crypto-js');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var message = 'I am user number 3';
-var hash = SHA256(message.toString());
+var password = '123abc';
 
-console.log(`Message: ${message}`);
-console.log(`Hash: ${hash}`);
+//generate salt
+//genSalt(number of rounds to generate the salt, callback (err, salt))
+bcrypt.genSalt(10, (err, salt) => {
+    bcrypt.hash(password, salt, (err, hash) => {
+      console.log(hash);
+    });
+});
 
+hashedPassword = '$2a$10$2.TuGOjs9EVEgtGbdWdlPOnsrKdpeqGMUaJQD.PKMvY9aIUYKaQjy';
 
-var data = {
-  id: 3
-};
-
-var token = {
-  data,
-  hash: SHA256(JSON.stringify(data) + 'somesecret').toString()
-}
-
-token.data.id = 5;
-token.hash = SHA256(JSON.stringify(token.data)).toString();
-
-var resultHash = SHA256(JSON.stringify(token.data) + 'somesecret').toString();
-
-if(resultHash === token.hash) {
-  console.log('Data was not changed');
-} else {
-  console.log('Data was changed. Do not trust!');
-}
+bcrypt.compare(password, hashedPassword, (err, res) => {
+  console.log(res);
+});
